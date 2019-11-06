f72ed1c17f9ecf59edcc47d5a353716744a2670c
monifra <monia.frankowska@gmail.com>
2019-11-06 14:59:38 +0100

    Update script.js
    adding additional features like search option

--- js/script.js
+++ js/script.js
@@ -14,6 +14,55 @@ const studentList = document.querySelectorAll('li'); //variable storing list of 
 const perPage = 10; //variable storing the number of items to show on page at one time
 
 /***
+Crating the search bar
+***/
+
+const headerStudentSearch = document.getElementsByClassName('page-header')[0]; //selecting div with class page-header
+const divStudentSearch = document.createElement('div'); //creating new div
+divStudentSearch.className = 'student-search';
+const inputStudentSearch = document.createElement('input'); //creating an input for search
+inputStudentSearch.setAttribute('placeholder','Search for students...');
+const buttonStudentSearch = document.createElement('button'); //creting a button for search
+buttonStudentSearch.textContent = 'Search';
+divStudentSearch.appendChild(inputStudentSearch);
+divStudentSearch.appendChild(buttonStudentSearch);
+headerStudentSearch.appendChild(divStudentSearch); //appending our new search bar to the existing div
+
+/***
+Creating the information that no matches were find by search and append it to the page
+***/
+
+const divNoResult = document.createElement('div');
+const pNoResult = document.createElement('p');
+pNoResult.textContent = 'No students were find';
+pNoResult.style.color = 'red';
+pNoResult.style.display = 'none';
+divStudentSearch.appendChild(pNoResult);
+
+/***
+Creating the no result information
+***/
+
+
+
+const searchIt = (searchInput, list) =>{
+  const searchResult = [];
+  for(let i =0; i< list.length; i++){
+
+    if(searchInput.value.length !== 0 && list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())){
+      list[i].style.display="";
+      searchResult.push(list[i]);
+      console.log(searchResult);
+      showPage(searchResult,1);
+      removePageLinks();
+      appendPageLinks(searchResult);
+    }else{
+      list[i].style.display="none";
+    }
+  }
+};
+
+/***
 showPage function hides all of the list items exept for those that you want to show on the page.
 ***/
 const showPage = (list,page) => { //list will be a list of student, page will be a number of page
@@ -56,7 +105,7 @@ const appendPageLinks = list =>{
 
     for(let i=0; i<= numberOfPages; i++){
       a[i].addEventListener('click', (e)=>{ // adding a listener for click on a tags
-        showPage(studentList,i+1); // when an 'a' tag is clicked showPage will choose a page with the right 'a' tag
+        showPage(list,i+1); // when an 'a' tag is clicked showPage will choose a page with the right 'a' tag
         for(let k=0; k<= numberOfPages; k++){
           a[k].className = ""; // leaving class name empty for all 'a' tags
           e.target.className = 'active'; // adding an 'active' class to an 'a' tag that was clicked
@@ -64,6 +113,33 @@ const appendPageLinks = list =>{
       });
     }
 };
+
+/***
+Remove pagination function
+***/
+
+const removePageLinks = () =>{
+  const divPage = document.querySelector('div.page'); //selecting parent
+  const divPagination = document.querySelector('div.pagination'); //selecting child
+  node.removeChild(divPagination);//removing child from parent
+};
+
+/***
+Event listener for search button
+***/
+
+buttonStudentSearch.addEventListener('click', (e) => {
+  e.preventDefault();
+  searchIt(inputStudentSearch,studentList);
+  console.log('Submit button is functional!'); //REMOVE
+});
+
+
+buttonStudentSearch.addEventListener('keyup', () => {
+  searchIt(inputStudentSearch,studentList);
+  console.log('Keyup event on the Search input is functional!'); //REMOVE
+});
+
 
 showPage(studentList,1); //running this function to show the first page of student list
 appendPageLinks(studentList);
